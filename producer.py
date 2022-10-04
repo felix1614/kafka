09db0f6a29fd443037bbaf8490eaa89ef0849e79
afnan <afnan@elmeasure.com>
2022-10-04 13:23:54 +0530

    kafka producer and consumer

--- producer.py
+++ producer.py
@@ -6,33 +6,33 @@ from kafka import KafkaProducer
 from datetime import datetime
 from dataGenerator import generateMessage
 
-myProducer = KafkaProducer(
-    bootstrap_servers='localhost:9092',
-    value_serializer=lambda x: dumps(x).encode('utf-8')
+# myProducer = KafkaProducer(
+#     bootstrap_servers='localhost:9092',
+#     value_serializer=lambda x: dumps(x).encode('utf-8')
+# )
+
+# while True:
+#     msgData = generateMessage()
+#     myProducer.send("messages", value=msgData)
+#     print("msg sent")
+#     sleep(5)
+
+
+def serializer(msg):
+    return dumps(msg).encode('utf-8')
+
+
+producer = KafkaProducer(
+    bootstrap_servers=['localhost:9092'],
+    value_serializer=serializer
 )
 
-while True:
-    msgData = generateMessage()
-    myProducer.send("messages", value=msgData)
-    print("msg sent")
-    sleep(5)
 
-#
-# def serializer(msg):
-#     return dumps(msg).encode('utf-8')
-#
-#
-# producer = KafkaProducer(
-#     bootstrap_servers=['localhost:9092'],
-#     value_serializer=serializer
-# )
-#
+if __name__ == "__main__":
+    while True:
+        dummy = generateMessage()
+        print(f"producing msg @ {datetime.now()} | message = {str(dummy)}")
+        producer.send('messages', dummy)
 
-# if __name__ == "__main__":
-#     while True:
-#         dummy = generateMessage()
-#         print(f"producing msg @ {datetime.now()} | message = {str(dummy)}")
-#         producer.send('messages', dummy)
-#
-#         sleepTime = random.randint(1, 11)
-#         time.sleep(sleepTime)
+        sleepTime = random.randint(1, 11)
+        time.sleep(sleepTime)
